03a1bfc921d5fd52e724b31e662e2d36eded9e80
Ismail Yusuf <ismailxyusuf@gmail.com>
2025-08-11 18:58:20 +0000

    now use public/contact.jpg as the background image as i still cant see i

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -182,7 +182,8 @@ export default function Home() {
       {/* Contact Section */}
       <section
         id="contact"
-        className="relative h-[600px] bg-cover bg-center bg-[url('/contact.jpg')]"
+        className="relative h-[600px] bg-cover bg-center"
+        style={{ backgroundImage: `url('/contact.jpg')` }}
       >
         <div className="absolute inset-0 bg-black/50 z-10" />
         <div className="relative z-20 flex h-full items-center justify-center p-4">
