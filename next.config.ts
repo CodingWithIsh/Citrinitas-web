0ff88bf62df67b04a71465319c39aaa7ce79b5cd
CodingWithIsh <79402740+CodingWithIsh@users.noreply.github.com>
2025-08-12 13:23:05 +0100

    Update next.config.ts
    For github pages "output: 'export'," added

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
@@ -19,5 +20,7 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+
+
 
 export default nextConfig;
