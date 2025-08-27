710fa2df8248d7494f296cf722e625c26642bb99
Ismail Yusuf <ismailxyusuf@gmail.com>
2025-08-27 12:03:37 +0000

    I want to use . Nunito Sans Analysis: This is likely the closest stylist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
       </head>
       <body className="font-body antialiased flex flex-col min-h-dvh">
